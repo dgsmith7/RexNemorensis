8b280bffc5229626fe70a954e57e5542fcd84397
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-12 13:52:20 -0600

    added even more todo items

--- src/RexNemorensis.java
+++ src/RexNemorensis.java
@@ -18,7 +18,10 @@ Comments
 Magic items messaging and efficacy - all five
 Add a probability - "you dropped youre weapon, buttefingers" when on an edge.
 Draw the map really nicely once at the beginning
+Draw an ascii image at the beginning
 Add a draw map command?
+Add some more scenery in useless areas
+Add some probabilty-driven variations in common text blurbs
 Refactor slop
 Consider making into a website just for fun in the vein of ZORK, design-wise
  */
